efb35f6f75eac7862b2000736dc203746c375f13
cajou42 <80094480+cajou42@users.noreply.github.com>
2024-04-29 11:35:23 +0200

    CustomSpringArm creation

---
+++ Source/Poladroid/Public/ActorComponents/CustomSpringArm.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "CustomSpringArm.generated.h"
+
+/**
+ * 
+ */
+UCLASS(meta=(BlueprintSpawnableComponent))
+class POLADROID_API UCustomSpringArm : public USpringArmComponent
+{
+	GENERATED_BODY()
+
+	virtual void GetTargetRotation();
+};
