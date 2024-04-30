9e19f94b0d22a0e9fbf35ab6f461aa1032beb5cf
HideNknow <outin.matthieu@gmail.com>
2024-04-30 16:44:25 +0200

    Add : StaminaDamages

---
+++ Source/Poladroid/Public/DamgeTypes/StaminaDamage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/DamageType.h"
+#include "StaminaDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class POLADROID_API UStaminaDamage : public UDamageType
+{
+	GENERATED_BODY()
+	
+};
